fb84b63d014e9874f6cb9a0b289bfc9d5eef793d
Nihal <nbaworks10010@gmail.com>
2026-01-19 10:39:56 +0530

    final commit

--- app.py
+++ app.py
@@ -8,15 +8,11 @@ def home():
     <html>
         <head>
             <title>Dockerized App</title>
-            <style>
-                body { font-family: sans-serif; text-align: center; padding-top: 50px; background-color: #f4f4f9; }
-                h1 { color: #333; }
-                .container { border: 1px solid #ddd; padding: 20px; display: inline-block; background: white; border-radius: 8px; }
-            </style>
+            
         </head>
         <body>
             <div class="container">
-                <h1>Hello from Docker! 🐳</h1>
+                <h1>Hello from Docker!</h1>
                 <p>This simple web application is running inside a container.</p>
             </div>
         </body>
